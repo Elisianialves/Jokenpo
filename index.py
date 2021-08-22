8f8fb317e89df66bf3d3ef112f4833ee30bcb73e
Elisiani <elisianialves@gmail.com>
2021-08-22 20:14:59 -0300

    Atualizado
    adicionei a opção sair e corrigi erros de jogada

--- index.py
+++ index.py
@@ -1,35 +1,51 @@
 from random import randint
 from time import sleep
 
-#add visualização do jogo
-print ('\nVAMOS JOGAR JOKENPO\n')
-acoes_do_jogo = ('','Pedra', 'Papel', 'Tesoura')
+# boas vindas
+print ('¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨')
+print ('                     VAMOS JOGAR JOKENPO                            ')
+print ('Pedra ganha de Tesoura, Tesoura ganha de Papel, Papel ganha de Pedra')
+print ('¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨\n')
+nome = input('Qual seu nome? ').capitalize()
+sleep (1)
+print ('{} Bem-Vindo(a) ao Jogo'.format(nome))
+sleep (1)
+
+# jogo
+acoes_do_jogo = ('','Pedra', 'Papel', 'Tesoura', 'Sair')
 bot = randint (1,3)
+print ('\nEscolha sua opção:')
 print (' [1] Pedra\n [2] Papel\n [3] Tesoura\n')
+print ('OU APERTE 4 PARA SAIR\n ')
+sleep (1)
 jogador = int(input('Qual é sua jogada? '))
+while jogador not in (1,2,3,4):
+    jogador = int (
+        input('JOGADA INVÁLIDA!. Escolha novamente ou precione 4 para sair '))
+else:
+    if jogador ==4:
+        print ('\n Obrigado por participar do Jogo.')        
 print ('\nJO')
 sleep (1)
 print ('KEN')
 sleep (1)
-print ('PO!')
+print ('PO!!!')
 sleep (1)
 print ('\nRobô jogou {}'.format(acoes_do_jogo[bot]))
 print ('Você jogou {}'.format(acoes_do_jogo[jogador]))
 
 #add regras do jogo
 if bot ==1 and jogador == 2:
-    print ('VOCÊ GANHOU!')
+    print ('PARABÉNS VOCÊ GANHOU!')
 elif bot ==2 and jogador ==1:
     print ('VOCÊ PERDEU!')
 elif bot ==3 and jogador ==1:
-    print ('VOCÊ GANHOU!')
+    print ('PARABÉNS VOCÊ GANHOU!')
 elif bot ==1 and jogador == 3:
     print ('VOCÊ PERDEU!')
 elif bot ==2 and jogador ==3:
-    print ('VOCÊ GANHOU!')
+    print ('PARABÉNS VOCÊ GANHOU!')
 elif bot ==3 and jogador ==2:
     print ('VOCÊ PERDEU!')
 elif bot == jogador:
     print ('EMPATOU!')
-else :
-    print ('JOGADA INVÁLIDA!')
